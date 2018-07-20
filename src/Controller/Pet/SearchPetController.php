442129522562c64e4cfc859d47a4de2b18064c84
Mitchel Vroege <19216221+MitchelAnthony@users.noreply.github.com>
2018-07-20 22:50:12 +0200

    Added skeleton search controller

---
+++ src/Controller/Pet/SearchPetController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Pet;
+
+use App\Controller\AbstractController;
+use App\Entity\Pet\Pet;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class SearchPetController extends AbstractController
+{
+    /**
+     * @Route("/pet/search/{id}", name="petregister_pet_search")
+     *
+     * @ParamConverter("pet", class="App\Entity\Pet\Pet", options={"mapping": {"id": "microchipId"}})
+     */
+    public function __invoke(Request $request, Pet $pet = null): Response
+    {
+        return new Response('<body></body>');
+    }
+}
