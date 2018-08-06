7a2b5d2d298658e9c9dc518809e8a84720a7d26a
Mitchel Vroege <19216221+MitchelAnthony@users.noreply.github.com>
2018-08-06 23:56:13 +0200

    Messages can now be sent to pet owners

--- src/Controller/Pet/FoundPetController.php
+++ src/Controller/Pet/FoundPetController.php
@@ -10,9 +10,11 @@ use App\Enum\Preference;
 use App\Form\NotificationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 final class FoundPetController extends AbstractController
 {
@@ -21,12 +23,11 @@ final class FoundPetController extends AbstractController
      *
      * @ParamConverter("pet", class="App\Entity\Pet\Pet")
      */
-    public function __invoke(Request $request, Pet $pet, FormFactoryInterface $formFactory): Response
+    public function __invoke(Request $request, Pet $pet, FormFactoryInterface $formFactory, RouterInterface $router): Response
     {
         $contacts = [];
         /** @var Contact $contact */
         foreach ($pet->getUser()->getContacts() as $contact) {
-            dump($contact->getPreference()->getValue());
             if ($contact->getPreference()->getValue() >= (new Preference(Preference::ALLOW_VISITORS))->getValue()) {
                 $contacts[] = $contact;
             }
@@ -38,7 +39,12 @@ final class FoundPetController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Implement form handling
+            $notification->setDate(new \DateTime());
+
+            $this->entityManager->persist($notification);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($router->generate('petregister_index'));
         }
 
         return new Response($this->twig->render('Pet/found.html.twig', [
